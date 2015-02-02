e3d3ba305c8494637a3b674fbe73377b1414049d
CVASL <CVASL@028-09584.ad.allstate.com>
2015-02-02 17:29:35 +0000

    2nd commit

--- mavenTest/src/test/java/mavenTest/TestApp1.java
+++ mavenTest/src/test/java/mavenTest/TestApp1.java
@@ -5,9 +5,16 @@ import junit.framework.Assert;
 import org.junit.Test;
 
  
+// TODO: Auto-generated Javadoc
+/**
+ * The Class TestApp1.
+ */
 @SuppressWarnings("deprecation")
 public class TestApp1 {
  
+	/**
+	 * Test print hello world.
+	 */
 	@Test
 	public void testPrintHelloWorld() {
  
