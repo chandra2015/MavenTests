cf3c990fca4ee8feb0baacaf78af06de028ac301
CVASL <CVASL@028-09584.ad.allstate.com>
2015-02-03 10:01:05 +0000

    Added java doc

--- mavenTest/src/test/java/mavenTest/TestApp2.java
+++ mavenTest/src/test/java/mavenTest/TestApp2.java
@@ -1,12 +1,21 @@
+/*
+ * This is sample Junit Test class
+ */
 package mavenTest;
 
 import junit.framework.Assert;
 
 import org.junit.Test;
  
+/**
+ * The Class TestApp2.
+ */
 @SuppressWarnings("deprecation")
 public class TestApp2 {
  
+	/**
+	 * Test print hello world2.
+	 */
 	@Test
 	public void testPrintHelloWorld2() {
  
